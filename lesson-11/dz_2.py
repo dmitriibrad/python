8ce4eedb5e92dc592c9576ba1505ca1e388f9711
dmitriibrad <bradutsanu@yandex.ru>
2022-05-03 19:27:10 +0300

    lesson-11_home-work

---
+++ lesson-11/dz_2.py
@@ -0,0 +1,23 @@
+class MyZeroDiv(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+if __name__ == "__main__":
+    from sys import exit
+
+    a = 0
+    b = 0
+    try:
+        a = float(input("input a: "))
+        b = float(input("input b: "))
+    except:
+        print("incorrect input")
+        exit(1)
+
+    try:
+        if b == 0:
+            raise MyZeroDiv("We try div by zero")
+        print(f"All is ok {a}/{b} = {a / b}")
+    except MyZeroDiv as ex:
+        print(ex)
